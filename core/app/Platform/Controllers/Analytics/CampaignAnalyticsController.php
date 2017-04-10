f86a38d29642440c9e15711efb62a11472655e22
skokhuis <info@madewithpepper.com>
2017-04-10 21:02:55 +0200

    Queries added for "All campaigns" analytics selection

--- core/app/Platform/Controllers/Analytics/CampaignAnalyticsController.php
+++ core/app/Platform/Controllers/Analytics/CampaignAnalyticsController.php
@@ -79,18 +79,45 @@ class CampaignAnalyticsController extends \App\Http\Controllers\Controller {
 
     // Card views
     // Raw query because of this issue: https://github.com/laravel/framework/issues/18523
-    $stats_card_views = \DB::select("select DATE(created_at) as date, count(id) as views 
-      from `card_stats` 
-      where `user_id` = :user_id 
-      and exists (select * from `campaigns` inner join `campaign_card` on `campaigns`.`id` = `campaign_card`.`campaign_id` where `campaign_card`.`campaign_id` in (:campaign_id)) 
-      and `created_at` >= :from and `created_at` <= :to 
-      group by DATE(created_at)", 
-    [
-      'user_id' => Core\Secure::userId(),
-      'campaign_id' => $campaign_id,
-      'from' => $from,
-      'to' => $to
-    ]);
+    if (is_numeric($campaign_id)) {
+      $stats_card_views = \DB::select("select DATE(created_at) as date, count(id) as views 
+        from `card_stats` 
+        where `user_id` = :user_id 
+        and exists (select * from `campaigns` inner join `campaign_card` on `campaigns`.`id` = `campaign_card`.`campaign_id` where `campaign_card`.`campaign_id` in (:campaign_id)) 
+        and `created_at` >= :from and `created_at` <= :to 
+        group by DATE(created_at)", 
+      [
+        'user_id' => Core\Secure::userId(),
+        'campaign_id' => $campaign_id,
+        'from' => $from,
+        'to' => $to
+      ]);
+    } elseif(is_array($campaign_id)) {
+      $campaign_ids = implode(',', $campaign_id);
+      $stats_card_views = \DB::select("select DATE(created_at) as date, count(id) as views 
+        from `card_stats` 
+        where `user_id` = :user_id 
+        and exists (select * from `campaigns` inner join `campaign_card` on `campaigns`.`id` = `campaign_card`.`campaign_id` where `campaign_card`.`campaign_id` in (:campaign_ids)) 
+        and `created_at` >= :from and `created_at` <= :to 
+        group by DATE(created_at)", 
+      [
+        'user_id' => Core\Secure::userId(),
+        'campaign_ids' => $campaign_ids,
+        'from' => $from,
+        'to' => $to
+      ]);
+    }  else {
+      $stats_card_views = \DB::select("select DATE(created_at) as date, count(id) as views 
+        from `card_stats` 
+        where `user_id` = :user_id 
+        and `created_at` >= :from and `created_at` <= :to 
+        group by DATE(created_at)", 
+      [
+        'user_id' => Core\Secure::userId(),
+        'from' => $from,
+        'to' => $to
+      ]);
+    }
 
     /*
     $stats_card_views = ModelAnalytics\CardStat::where('user_id', Core\Secure::userId())
@@ -105,16 +132,34 @@ class CampaignAnalyticsController extends \App\Http\Controllers\Controller {
       ->toArray();
     */
 
-    //dd(\DB::getQueryLog()); 
     // Interactions
-    $stats_interactions = Location\Interaction::where('user_id', Core\Secure::userId())
-      ->select(\DB::raw('DATE(created_at) as date'), \DB::raw('count(id) as interactions'))
-      ->where('campaign_id', $campaign_id)
-      ->where('created_at', '>=', $from)
-      ->where('created_at', '<=', $to)
-      ->groupBy([\DB::raw('DATE(created_at)')])
-      ->get()
-      ->toArray();
+    if (is_numeric($campaign_id)) {
+      $stats_interactions = Location\Interaction::where('user_id', Core\Secure::userId())
+        ->select(\DB::raw('DATE(created_at) as date'), \DB::raw('count(id) as interactions'))
+        ->where('campaign_id', $campaign_id)
+        ->where('created_at', '>=', $from)
+        ->where('created_at', '<=', $to)
+        ->groupBy([\DB::raw('DATE(created_at)')])
+        ->get()
+        ->toArray();
+    } elseif(is_array($campaign_id)) {
+      $stats_interactions = Location\Interaction::where('user_id', Core\Secure::userId())
+        ->select(\DB::raw('DATE(created_at) as date'), \DB::raw('count(id) as interactions'))
+        ->whereIn('campaign_id', $campaign_id)
+        ->where('created_at', '>=', $from)
+        ->where('created_at', '<=', $to)
+        ->groupBy([\DB::raw('DATE(created_at)')])
+        ->get()
+        ->toArray();
+    } else {
+      $stats_interactions = Location\Interaction::where('user_id', Core\Secure::userId())
+        ->select(\DB::raw('DATE(created_at) as date'), \DB::raw('count(id) as interactions'))
+        ->where('created_at', '>=', $from)
+        ->where('created_at', '<=', $to)
+        ->groupBy([\DB::raw('DATE(created_at)')])
+        ->get()
+        ->toArray();
+    }
 
     // Create range
     $interaction_range = Analytics\AnalyticsController::getRange($date_start, $date_end);
